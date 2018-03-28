84dadabbae400d701e44efd6aaa600a40c7de2e1
Vinodh-G <vinodh.g.in@gmail.com>
2018-03-28 10:52:36 +0530

    Used URLSession to download cluster data

--- SuperKomputer-Mac/SuperKomputer-Mac/APICore/APICore+ClusterList.swift
+++ SuperKomputer-Mac/SuperKomputer-Mac/APICore/APICore+ClusterList.swift
@@ -14,16 +14,49 @@ struct ClustersRequestParam {
 
 struct ClusterResponseParam {
     var success: Bool
-    var clusters: [Cluster]
-    var error: Error
+    var clusters: [Cluster]?
+    var error: Error?
 }
-
-let kClustersServerAPI = "\(kBaseURL)/users/{username}/clusters"
 
 typealias getClustersBlock = (_ responseParam: ClusterResponseParam) -> Void
 
 extension APICore {
-    func getClusters(requestParam: ClustersRequestParam, callBack:getClustersBlock) {
+    func getClusters(requestParam: ClustersRequestParam, callBack:@escaping getClustersBlock) {
+     
+        let session = URLSession.shared
+        let urlString = kBaseURL.appending("/users/\(requestParam.userId))/clusters")
+        
+        guard let url = URL(string: urlString)  else {
+            let response = ClusterResponseParam(success: false,
+                                                clusters: nil,
+                                                error: nil)
+            callBack(response)
+            return
+        }
+        
+        
+        let dataTask = session.dataTask(with: url, completionHandler: { (data, response, error) in
+            
+            guard let newData = data else {
+                let response = ClusterResponseParam(success: false,
+                                                    clusters: nil,
+                                                    error: nil)
+                callBack(response)
+                return
+            }
+            
+            let json = try? JSONSerialization.jsonObject(with: newData, options: [])
+            
+            if let jsonResult = json as? [String:Any] {
+                print(jsonResult)
+                let clusters = ClusterFactory.getClusters(from: jsonResult)
+                let response = ClusterResponseParam(success: true,
+                                                    clusters: clusters,
+                                                    error: nil)
+                callBack(response)
+            }
+        })
+        dataTask.resume()
         
     }
 }
