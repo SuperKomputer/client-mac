0e3b1004dbeffabce1b3bfa2d572bd4b3e6ac17a
Damodar Shenoy <itsdamslife@gmail.com>
2018-03-28 12:57:28 +0530

    Connected UI elements to the code, refactored navigation

--- SuperKomputer-Mac/SuperKomputer-Mac/DashboardViewController.swift
+++ SuperKomputer-Mac/SuperKomputer-Mac/DashboardViewController.swift
@@ -10,9 +10,30 @@ import Cocoa
 
 class DashboardViewController: NSViewController {
 
+    @IBOutlet weak var rentBtn: NSButton!
+    @IBOutlet weak var requestBtn: NSButton!
+    
+    @IBOutlet weak var container: NSBox!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        self.view.wantsLayer = true
+        self.view.layer?.backgroundColor = NSColor.white.cgColor
+        
+        rentBtn.setWhiteTitle("Rent a cluster")
+        requestBtn.setWhiteTitle("Request a cluster")
+        
+        AppDelegate.instance.currentViewController = self
     }
     
+    @IBAction func rentACluster(_ sender: Any) {
+        
+    }
+    
+    @IBAction func requestACluster(_ sender: Any) {
+        
+    }
+
 }
