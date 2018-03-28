34ba1556ef7295720c4a80c51cd69006bd706936
Damodar Shenoy <itsdamslife@gmail.com>
2018-03-28 12:16:57 +0530

    Added a container dashboard view with rent and request button

---
+++ SuperKomputer-Mac/SuperKomputer-Mac/DashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardViewController.swift
+//  SuperKomputer-Mac
+//
+//  Created by Damodar Shenoy on 3/28/18.
+//  Copyright © 2018 SuperKomputer. All rights reserved.
+//
+
+import Cocoa
+
+class DashboardViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
