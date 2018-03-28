a3ae1f1de9c0a1e35af5001386bcd2063f79e382
Damodar Shenoy <itsdamslife@gmail.com>
2018-03-28 16:08:35 +0530

    Added cluster list datasource and delegates

--- SuperKomputer-Mac/SuperKomputer-Mac/RentViewController.swift
+++ SuperKomputer-Mac/SuperKomputer-Mac/RentViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class RentViewController: NSViewController {
+class RentViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet weak var clusterListView: NSTableView!
     
@@ -17,15 +17,34 @@ class RentViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        clusterListView.delegate = self
+        clusterListView.dataSource = self
+        
         self.view.wantsLayer = true
         self.view.layer?.backgroundColor = NSColor(red: 91.0, green: 211.0, blue: 153.0, alpha: 1.0).cgColor
+        
+    }
+    
+    override func viewDidAppear() {
+        clusterListView.reloadData()
     }
     
     @IBAction func rentMyCluster(_ sender: Any) {
-        
+        clusterListView.reloadData()
     }
     
     @IBAction func stop(_ sender: Any) {
         
     }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any?  {
+        let s: String = "r:\(row)/c::\(tableColumn!.identifier.rawValue)"
+        return s
+    }
+
 }
--- SuperKomputer-Mac/SuperKomputer-Mac/RequestViewController.swift
+++ SuperKomputer-Mac/SuperKomputer-Mac/RequestViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class RequestViewController: NSViewController {
+class RequestViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet weak var clusterListView: NSTableView!
     @IBOutlet weak var requestBtn: NSButton!
@@ -16,6 +16,10 @@ class RequestViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        clusterListView.delegate = self
+        clusterListView.dataSource = self
+        
+        
         self.view.wantsLayer = true
         self.view.layer?.backgroundColor = NSColor(red: 226.0, green: 180.0, blue: 247.0, alpha: 1.0).cgColor
         self.clusterListView.backgroundColor = NSColor(red: 226.0, green: 180.0, blue: 247.0, alpha: 1.0)
@@ -27,5 +31,15 @@ class RequestViewController: NSViewController {
     
     @IBAction func stopCluster(_ sender: Any) {
         
+    }
+    
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        let s: String = "r:\(row)/c::\(tableColumn!.identifier.rawValue)"
+        return s
     }
 }
